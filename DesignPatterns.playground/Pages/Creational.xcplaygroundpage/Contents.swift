4fca0c1d5171dc2ca71306134fcd09694dbb19eb
Alexis Ortega <alexis.oc3@gmail.com>
2018-03-06 18:23:30 -0700

    Add singleton pattern

---
+++ DesignPatterns.playground/Pages/Creational.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+Singleton
+------------
+
+The singleton pattern ensures that only one object of a particular class is ever created.
+All further references to objects of the singleton class refer to the same underlying instance.
+There are very few applications, do not overuse this pattern!
+
+### Example:
+*/
+class Singleton {
+    static let `default` = Singleton()
+    
+    private init() {
+        // Private initialization to ensure just one instance is created.
+    }
+}
+ 
+//: [Next](@next)
