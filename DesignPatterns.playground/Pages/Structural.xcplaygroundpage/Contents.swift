34810b93bbdab94a1d13d7dac62eb34d90914751
Alexis Ortega <alexis.oc3@gmail.com>
2018-03-08 19:19:26 -0700

    Add decorator pattern

---
+++ DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+//: [Previous](@previous)
+
+/*:
+ Decorator
+ ------------
+ 
+ The decorator pattern is used to extend or alter the functionality of objects at run- time by wrapping them in an object of a decorator class.
+ This provides a flexible alternative to using inheritance to modify behaviour.
+ 
+ ### Example
+ */
+protocol Element {
+    func imagePath() -> String
+}
+
+class Shape: Element {
+    func imagePath() -> String {
+        return "Shape.png"
+    }
+}
+
+class Number: Element {
+    func imagePath() -> String {
+        return "Number.png"
+    }
+}
+
+class ElementDecorator: Element {
+    private let decoratedElement: Element
+    fileprivate let ingredientSeparator: String = ", "
+    
+    required init(decoratedCoffee: Element) {
+        self.decoratedCoffee = decoratedCoffee
+    }
+    
+    func imagePath() -> String {
+        return decoratedCoffee.imagePath()
+    }
+}
+
+final class ColorDecorator: ElementDecorator {
+    required init(decoratedElement: Element) {
+        super.init(decoratedElement: decoratedElement)
+    }
+    
+    override func imagePath() -> String {
+        return "color/" + super.imagePath()
+    }
+}
+
+final class SizeDecorator: ElementDecorator {
+    required init(decoratedElement: Element) {
+        super.init(decoratedElement: decoratedElement)
+    }
+    
+    override func imagePath() -> String {
+        return "size/" + super.imagePath()
+    }
+}
+/*:
+ ### Usage:
+ */
+var element: Element = Shape()
+print("Path = \(element.imagePath())")
+element = ColorDecorator(decoratedElement: element)
+print("Path = \(element.imagePath())")
+element = SizeDecorator(decoratedElement: element)
+print("Path = \(element.imagePath())")
+
+//: [Next](@next)
