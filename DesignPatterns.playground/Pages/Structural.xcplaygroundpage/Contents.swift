9ac3a44352cae94f4db57e244920d10d1b7fa621
Alexis Ortega <alexis.oc3@gmail.com>
2018-06-28 19:09:29 -0600

    Add adapter pattern

--- DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
@@ -12,6 +12,65 @@
 import Swift
 import Foundation
 /*:
+ 🔌 Adapter
+ ----------
+ 
+ The adapter pattern is used to provide a link between two otherwise incompatible types by wrapping the "adaptee" with a class that supports the interface required by the client.
+ 
+ ### Example
+ */
+
+class XMLParser {
+    private let response: XMLResponse
+    
+    init(response: XMLResponse) {
+        self.response = response
+    }
+    
+    func parse() -> JSONResponse {
+        return JSONResponse()
+    }
+}
+
+class JSONResponse {
+    let value = "[{}]"
+}
+
+class XMLResponse {
+    let value = "<?xml version=\"1.0\" ?><note></note>"
+}
+
+protocol HTTPRequestTarget {
+    func request() -> JSONResponse
+}
+
+class HTTPClient: HTTPRequestTarget {
+    private let adaptee: LegacyHTTPClient
+    
+    init(_ adaptee: LegacyHTTPClient) {
+        self.adaptee = adaptee
+    }
+    
+    func request() -> JSONResponse {
+        let xmlResponse = adaptee.request()
+        let jsonResponse = XMLParser(response: xmlResponse)
+        return jsonResponse.parse()
+    }
+}
+
+class LegacyHTTPClient {
+    func request() -> XMLResponse {
+        return XMLResponse()
+    }
+}
+/*:
+ ### Usage
+ */
+let adaptee = LegacyHTTPClient()
+let adapter = HTTPClient(adaptee: adaptee)
+
+adapter.request()
+/*:
  Composite
  ----------
  
