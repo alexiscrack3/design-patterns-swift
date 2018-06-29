d2c13eafa3a364e85d722c2b445187fc035ca554
Alexis Ortega <alexis.oc3@gmail.com>
2018-06-29 00:26:43 -0600

    Add Protection Proxy

--- DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
@@ -281,3 +281,56 @@ element = ColorDecorator(decoratedElement: element)
 print("Path = \(element.imagePath())")
 element = SizeDecorator(decoratedElement: element)
 print("Path = \(element.imagePath())")
+/*:
+ Protection Proxy
+ -----------------
+ 
+ The proxy pattern is used to provide a surrogate or placeholder object, which references an underlying object.
+ Protection proxy is restricting access.
+ 
+ ### Example
+ */
+struct Resource {
+    let id: String
+}
+
+protocol Authenticable {
+    func getResourceById(_ id: String) -> Resource?
+}
+
+class ResourceManager: Authenticable {
+    let resources = [Resource(id: "1"), Resource(id: "2"), Resource(id: "3")]
+    func getResourceById(_ id: String) -> Resource? {
+        return self.resources.first(where: { $0.id == id })
+    }
+}
+
+class VaultManager: Authenticable {
+    private var resourceManager: ResourceManager!
+    
+    func authenticate(password: String) -> Bool {
+        guard password == "pass" else {
+            return false
+        }
+        
+        resourceManager = ResourceManager()
+        
+        return true
+    }
+    
+    func getResourceById(_ id: String) -> Resource? {
+        guard resourceManager != nil else {
+            return nil
+        }
+        
+        return resourceManager.getResourceById(id)
+    }
+}
+/*:
+ ### Usage
+ */
+let vaultManager = VaultManager()
+_ = vaultManager.getResourceById("123")
+
+_ = vaultManager.authenticate(password: "pass")
+_ = vaultManager.getResourceById("1")
