62f222b681b8c38ea1d066e4e8240545e85fa32a
Alexis Ortega <alexis.oc3@gmail.com>
2018-06-28 23:49:50 -0600

    Add factory method pattern

--- DesignPatterns.playground/Pages/Creational.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Creational.xcplaygroundpage/Contents.swift
@@ -129,6 +129,82 @@ let motorcycleBuilder = MotorcycleBuilder()
 let motorcycle = Shop().build(builder: motorcycleBuilder)
 print(motorcycle)
 /*:
+ Factory Method
+ ---------------
+ 
+ The factory pattern is used to replace class constructors, abstracting the process of object generation so that the type of the object instantiated can be determined at run-time.
+ 
+ ### Example
+ */
+
+protocol Shoe {
+    var price: Double { get set }
+    var weight: Float { get set }
+    var type: String { get set }
+}
+
+protocol Ball {
+    var price: Double { get set }
+}
+
+struct RunningShoe: Shoe {
+    var price: Double
+    var weight: Float
+    var type: String
+}
+
+struct SoccerBall: Ball {
+    var price: Double
+}
+
+struct BasketballBall: Ball {
+    var price: Double
+}
+
+protocol SportsFactory {
+    func makeShoe() -> Shoe
+    func makeSoccerBall() -> Ball
+    func makeBasketballBall() -> Ball
+}
+
+class NikeFactory: SportsFactory {
+    func makeShoe() -> Shoe {
+        return RunningShoe(price: 100.0, weight: 11.4, type: "Neutral")
+    }
+    
+    func makeSoccerBall() -> Ball {
+        return SoccerBall(price: 80)
+    }
+    
+    func makeBasketballBall() -> Ball {
+        return BasketballBall(price: 50)
+    }
+}
+
+class AdidasFactory: SportsFactory {
+    func makeShoe() -> Shoe {
+        return RunningShoe(price: 200.0, weight: 11.0, type: "Neutral")
+    }
+    
+    func makeSoccerBall() -> Ball {
+        return SoccerBall(price: 100)
+    }
+    
+    func makeBasketballBall() -> Ball {
+        return BasketballBall(price: 60)
+    }
+}
+/*:
+ ### Usage
+ */
+let creators: [SportsFactory] = [NikeFactory(), AdidasFactory()]
+for creator in creators {
+    let soccerBall = creator.makeSoccerBall()
+    let basketballBall = creator.makeBasketballBall()
+    print(soccerBall.price)
+    print(basketballBall.price)
+}
+/*:
  Singleton
  ----------
 
