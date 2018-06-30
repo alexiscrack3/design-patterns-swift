3e06c1ff5cf9cf648776a5b7be668dd0c9db55ea
Alexis Ortega <alexis.oc3@gmail.com>
2018-06-30 12:59:10 -0600

    Add template method

--- DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
@@ -120,3 +120,72 @@ docFile.save("file")
 
 let textFile = SaveFileDialog(strategy: TextFileStrategy())
 textFile.save("file")
+/*:
+ 🍪 Template
+ -----------
+ 
+ The Template Pattern is used when two or more implementations of an
+ algorithm exist. The template is defined and then built upon with further
+ variations. Use this method when most (or all) subclasses need to implement
+ the same behavior. Traditionally, this would be accomplished with abstract
+ classes and protected methods (as in Java). However in Swift, because
+ abstract classes don't exist (yet - maybe someday),  we need to accomplish
+ the behavior using interface delegation.
+ 
+ ### Example
+ */
+protocol BoardGame {
+    func play()
+}
+
+protocol BoardGamePhases {
+    func initialize()
+    func start()
+}
+
+class BoardGameController: BoardGame {
+    private var delegate: BoardGamePhases
+    
+    init(delegate: BoardGamePhases) {
+        self.delegate = delegate
+    }
+    
+    private func openBox() {
+        // common implementation
+        print("BoardGameController openBox() executed")
+    }
+    
+    final func play() {
+        openBox()
+        delegate.initialize()
+        delegate.start()
+    }
+}
+
+class Monoply: BoardGamePhases {
+    func initialize() {
+        print("Monoply initialize() executed")
+    }
+    
+    func start() {
+        print("Monoply start() executed")
+    }
+}
+
+class Battleship: BoardGamePhases {
+    func initialize() {
+        print("Battleship initialize() executed")
+    }
+    
+    func start() {
+        print("Battleship start() executed")
+    }
+}
+/*:
+ ### Usage
+ */
+let monoply = BoardGameController(delegate: Monoply())
+monoply.play();
+
+let battleship = BoardGameController(delegate: Battleship())
+battleship.play();
