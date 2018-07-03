e1359dcde65e25a11a0a302b6749b5a2383f8968
Alexis Ortega <alexis.oc3@gmail.com>
2018-07-02 19:24:14 -0600

    Add façade pattern

--- DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
@@ -283,6 +283,67 @@ print("Path = \(element.imagePath())")
 element = SizeDecorator(decoratedElement: element)
 print("Path = \(element.imagePath())")
 /*:
+ Façade
+ -------
+ 
+ The facade pattern is used to define a simplified interface to a more complex subsystem.
+ 
+ ### Example
+ */
+protocol FlightBooking {
+    func book()
+}
+
+class FlightBookingSystem: FlightBooking {
+    func book() {
+        print("Flight booked successfully")
+    }
+}
+
+protocol HotelBooking {
+    func book()
+}
+
+class HotelBookingSystem: HotelBooking {
+    func book() {
+        print("Hotel booked successfully")
+    }
+}
+
+protocol TrasferBooking {
+    func book()
+}
+
+class TrasferBookingSystem: TrasferBooking {
+    func book() {
+        print("Transfer booked successfully")
+    }
+}
+
+protocol TravelPackage {
+    func book()
+}
+
+class TravelPackageFacade: TravelPackage {
+    func book() {
+        let trasferBooking = TrasferBookingSystem()
+        trasferBooking.book()
+        
+        let hotelBooking = HotelBookingSystem()
+        hotelBooking.book()
+        
+        let flightBooking = FlightBookingSystem()
+        flightBooking.book()
+        
+        print("Travel package booked successfully")
+    }
+}
+/*:
+ ### Usage
+ */
+let travelPackage = TravelPackageFacade()
+travelPackage.book()
+/*:
  Protection Proxy
  -----------------
  
