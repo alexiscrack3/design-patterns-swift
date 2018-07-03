571bfab2af8dd09cf014d477e36cd7f58b16c1d4
Alexis Ortega <alexis.oc3@gmail.com>
2018-07-03 00:05:03 -0600

    Add iterator pattern

--- DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
@@ -36,6 +36,73 @@ class ImmutablePerson {
 let person = ImmutablePerson(name: "Foo")
 person.uppercased()
 /*:
+ Iterator
+ ---------
+ 
+ The iterator pattern is used to provide a standard interface for traversing a collection of items in an aggregate object without the need to understand its underlying structure.
+ 
+ ### Example:
+ */
+struct Song {
+    let title: String
+}
+
+protocol MusicLibrary: Sequence {
+    var songs: [Song] { get }
+}
+
+class MusicLibraryIterator: IteratorProtocol {
+    private var current = 0
+    private let songs: [Song]
+    
+    init(songs: [Song]) {
+        self.songs = songs
+    }
+    
+    func next() -> Song? {
+        defer { current += 1 }
+        return songs.count > current ? songs[current] : nil
+    }
+}
+
+class PandoraIterator: MusicLibraryIterator {
+}
+
+class SpotifyIterator: MusicLibraryIterator {
+}
+
+class Pandora: MusicLibrary {
+    var songs: [Song]
+    
+    init(songs: [Song]) {
+        self.songs = songs
+    }
+    
+    func makeIterator() -> MusicLibraryIterator {
+        return PandoraIterator(songs: songs)
+    }
+}
+
+class Spotify: MusicLibrary {
+    var songs: [Song]
+    
+    init(songs: [Song]) {
+        self.songs = songs
+    }
+    
+    func makeIterator() -> MusicLibraryIterator {
+        return SpotifyIterator(songs: songs)
+    }
+}
+/*:
+ ### Usage
+ */
+let spotify = Spotify(songs: [Song(title: "Foo"), Song(title: "Bar")] )
+
+for song in spotify {
+    print("I've read: \(song)")
+}
+/*:
  Observer
  ---------
  
