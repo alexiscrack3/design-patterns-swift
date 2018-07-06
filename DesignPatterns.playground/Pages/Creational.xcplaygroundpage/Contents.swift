4a8d1b5647c0c03ecc466cc7e82c77d883ba5dbc
Alexis Ortega <alexis.oc3@gmail.com>
2018-07-06 00:13:12 -0600

    Add prototype pattern

--- DesignPatterns.playground/Pages/Creational.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Creational.xcplaygroundpage/Contents.swift
@@ -11,6 +11,7 @@
  */
 import Swift
 import Foundation
+import UIKit
 /*:
  Abstract Factory
  -----------------
@@ -410,6 +411,51 @@ _ = group.wait(timeout: .distantFuture)
 _ = MagicHouse.lendMagicObject(occupier: "William")
 _ = MagicHouse.lendMagicObject(occupier: "Tato")
 MagicHouse.printReport()
+/*:
+ Prototype
+ ----------
+ 
+ The prototype pattern is used to instantiate a new object by copying all of the properties of an existing object, creating an independent clone.
+ This practise is particularly useful when the construction of a new object is inefficient.
+ 
+ ### Example
+ */
+protocol TextScheme {
+    var font: UIFont { get set }
+    var color: UIColor { get set }
+    var text: String { get set}
+    func clone() -> Paragraph
+}
+
+class Paragraph: TextScheme {
+    var font: UIFont
+    var color: UIColor
+    var text: String
+    
+    init(font: UIFont = UIFont.systemFont(ofSize: 12), color: UIColor = .darkText, text: String = "") {
+        self.font = font
+        self.color = color
+        self.text = text
+    }
+    
+    func clone() -> Paragraph {
+        return Paragraph(font: self.font, color: self.color, text: self.text)
+    }
+}
+/*:
+ ### Usage
+ */
+let base = Paragraph()
+
+let title = base.clone()
+title.font = UIFont.systemFont(ofSize: 18)
+title.text = "This is the title"
+
+let first = base.clone()
+first.text = "This is the first paragraph"
+
+let second = base.clone()
+second.text = "This is the second paragraph"
 /*:
  Singleton
  ----------
