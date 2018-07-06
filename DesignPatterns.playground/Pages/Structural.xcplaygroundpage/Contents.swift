ff6817d30cff4a8e54488eeb77b23ea8a6098f98
Alexis Ortega <alexis.oc3@gmail.com>
2018-07-06 16:24:29 -0600

    Add private class data pattern

--- DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Structural.xcplaygroundpage/Contents.swift
@@ -71,6 +71,49 @@ let adaptee = LegacyHTTPClient()
 let adapter = HTTPClient(adaptee)
 
 adapter.request()
+
+/*:
+ Private Class Data
+ -------------------
+ The private class data design pattern seeks to reduce exposure of attributes by limiting their visibility.
+ 
+ ### Example
+ */
+private class CircleData {
+    var radius: Double
+    var color: UIColor
+    var origin: CGPoint
+    
+    init(radius: Double, color: UIColor, origin: CGPoint) {
+        self.radius = radius
+        self.color = color
+        self.origin = origin
+    }
+}
+
+class Circle {
+    private let circleData: CircleData
+    
+    init(radius: Double, color: UIColor, origin: CGPoint) {
+        self.circleData = CircleData(radius: radius, color: color, origin: origin)
+    }
+    
+    var circumference: Double {
+        return circleData.radius * Double.pi
+    }
+    
+    var diameter: Double {
+        return circleData.radius * 2
+    }
+    
+    func draw(graphics: Data) {
+    }
+}
+/*:
+ ### Usage
+ */
+let circle = Circle(radius: 3.0, color: .red, origin: .zero)
+print(circle.circumference)
 /*:
  Composite
  ----------
