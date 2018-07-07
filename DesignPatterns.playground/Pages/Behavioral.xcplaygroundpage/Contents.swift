f2e5f2d8356ffe88a07873e43abea3189718fc52
Alexis Ortega <alexis.oc3@gmail.com>
2018-07-06 19:47:40 -0600

    Add visitor pattern

--- DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
@@ -255,7 +255,7 @@ class Computer {
     }
     
     func undo(levels: Int) {
-        print("\n---- Undo \(levels) levels");
+        print("\n---- Undo \(levels) levels")
         
         var i = 0
         while i < levels {
@@ -893,7 +893,54 @@ class Battleship: BoardGamePhases {
  ### Usage
  */
 let monoply = BoardGameController(delegate: Monoply())
-monoply.play();
+monoply.play()
 
 let battleship = BoardGameController(delegate: Battleship())
-battleship.play();
+battleship.play()
+/*:
+ Visitor
+ --------
+ 
+ The visitor pattern is used to separate a relatively complex set of structured data classes from the functionality that may be performed upon the data that they hold.
+ 
+ ### Example
+ */
+protocol PlanetVisitor {
+    func visit(planet: Earth)
+    func visit(planet: Mars)
+    func visit(planet: Jupiter)
+}
+
+protocol Planet {
+    func accept(visitor: PlanetVisitor)
+}
+
+class Earth: Planet {
+    func accept(visitor: PlanetVisitor) { visitor.visit(planet: self) }
+}
+
+class Mars: Planet {
+    func accept(visitor: PlanetVisitor) { visitor.visit(planet: self) }
+}
+
+class Jupiter: Planet {
+    func accept(visitor: PlanetVisitor) { visitor.visit(planet: self) }
+}
+
+class NameVisitor: PlanetVisitor {
+    var name = ""
+    
+    func visit(planet: Earth)  { name = "Earth" }
+    func visit(planet: Mars) { name = "Mars" }
+    func visit(planet: Jupiter)  { name = "Jupiter" }
+}
+/*:
+ ### Usage
+ */
+let planets: [Planet] = [Earth(), Mars(), Jupiter()]
+
+let names = planets.map { (planet: Planet) -> String in
+    let visitor = NameVisitor()
+    planet.accept(visitor: visitor)
+    return visitor.name
+}
