58b466f814402f853bd8f1cd1973f55aa22075f5
Alexis Ortega <alexis.oc3@gmail.com>
2018-07-06 19:12:13 -0600

    Add memento pattern

--- DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
+++ DesignPatterns.playground/Pages/Behavioral.xcplaygroundpage/Contents.swift
@@ -618,6 +618,49 @@ let yellow = Light(color: "Yellow", lightMediator: lightMediator)
 red.turnOn()
 green.turnOn()
 yellow.turnOn()
+
+/*:
+ Memento
+ --------
+ 
+ The memento pattern is used to capture the current state of an object and store it in such a manner that it can be restored at a later time without breaking the rules of encapsulation.
+ 
+ ### Example
+ */
+
+class EditorMemento {
+    let editorState: String
+    
+    init(state: String) {
+        editorState = state
+    }
+    
+    func getSavedState() -> String {
+        return editorState
+    }
+}
+
+class Editor {
+    var contents = ""
+    
+    func save() -> EditorMemento {
+        return EditorMemento(state: contents)
+    }
+    
+    func restoreToState(memento: EditorMemento) {
+        contents = memento.getSavedState()
+    }
+}
+/*:
+ ### Usage
+ */
+let editor = Editor()
+editor.contents = "Foo"
+let memento = editor.save()
+
+editor.contents = "Bar"
+editor.restoreToState(memento: memento)
+editor.contents
 /*:
  Observer
  ---------
